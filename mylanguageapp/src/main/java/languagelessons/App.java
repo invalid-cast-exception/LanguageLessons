fd6b6b1cbb3f4f5540638f268de3a8c4ba7b06f6
invalid-cast-exception <159726535+invalid-cast-exception@users.noreply.github.com>
2024-06-11 19:34:09 +0300

    Rename the app - by changing the directory name and the corresponding string in settings.gradle.

--- mylanguageapp/src/main/java/languagelessons/App.java
+++ mylanguageapp/src/main/java/languagelessons/App.java
@@ -10,5 +10,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        System.out.printf("Java version of app = %s", Runtime.version());
+
     }
 }
