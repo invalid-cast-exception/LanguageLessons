30d35bf0979d259de5dc72bd1c515221d7d7c71a
invalid-cast-exception <159726535+invalid-cast-exception@users.noreply.github.com>
2024-06-11 19:44:10 +0300

    Add almost-empty Word class to get started. First test-driven-development step in Red-Green-Refactor cycle - a red test (in this case red due to not compiling until GetRootWord is implemented in the planned next step).

---
+++ mylanguageapp/src/test/java/languagelessons/WordTest.java
@@ -0,0 +1,16 @@
+package languagelessons;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class WordTest {
+    @Test void wordCanBeConstructed() {
+
+        Word myFirstWord = new Word();
+
+        assertNotNull(myFirstWord);
+        assertEquals(myFirstWord.GetRootWord(), "Nothing");
+
+    }
+}
