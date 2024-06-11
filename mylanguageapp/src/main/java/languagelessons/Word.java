dc1022a60f34beb4fe27d1eed5623689b8b411ed
invalid-cast-exception <159726535+invalid-cast-exception@users.noreply.github.com>
2024-06-11 19:50:32 +0300

    Add first implementation for getRootWord. Used VSCode's quick fix option to auto-generate getRootWord method on Word class. Also, changed first letter's case from upper to lower as per old advice on method naming.

--- mylanguageapp/src/main/java/languagelessons/Word.java
+++ mylanguageapp/src/main/java/languagelessons/Word.java
@@ -2,4 +2,11 @@ package languagelessons;
 
 public class Word {
 
+    public Object GetRootWord() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GetRootWord'");
+    }
+
+
+
 }
