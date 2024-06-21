31d5fc4ecd0c83708db859afd7a969bb391f6eee
invalid-cast-exception <159726535+invalid-cast-exception@users.noreply.github.com>
2024-06-22 01:17:38 +0300

    Add Vocabulary class. Each vocabulary contains a list of words, intended to separate dictionaries of learned words. Adds unit test class, with a constructor test passing and a failing test for addWords method.

---
+++ mylanguageapp/src/main/java/languagelessons/Vocabulary.java
@@ -0,0 +1,41 @@
+package languagelessons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//For more info about Java Record, since Java SE 14, see: https://docs.oracle.com/en/java/javase/17/language/records.html
+public class Vocabulary{
+
+    private String name;
+    private List<Word> wordsInVocabulary;
+
+    public Vocabulary(String nameOfVocabulary) {
+        
+        this.wordsInVocabulary = new ArrayList<>();
+        this.name = nameOfVocabulary;
+
+    }
+
+    public Boolean AddWord(Word wordToAdd){
+        return AddWord(wordToAdd, false);
+    }
+
+    public Boolean AddWord(Word wordToAdd, Boolean overwriteIfExists){
+
+        Boolean wasAdded = false;
+
+
+
+        return wasAdded;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getWordCount() {
+        return this.wordsInVocabulary.size();
+    }
+
+
+}
---
+++ mylanguageapp/src/test/java/languagelessons/VocabularyTest.java
@@ -0,0 +1,48 @@
+package languagelessons;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class VocabularyTest {
+
+
+    @Test void vocabularyCanBeConstructed(){
+
+        String nameOfVocabulary = "Work";
+        Vocabulary someWords = new Vocabulary(nameOfVocabulary);
+
+        assertNotNull(someWords);
+        assertEquals(nameOfVocabulary, someWords.getName());
+        
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={
+        "a,b,c"
+        , ""
+        , "b"
+    })
+    void wordsCanBeAddedToVocabulary(String sampleWordsAsCommaDelimitedString){
+
+        String[] wordsToAdd = sampleWordsAsCommaDelimitedString.split(",");
+
+        Vocabulary someWords = new Vocabulary("FancyWords");
+
+        //defensive check before the test starts - we should see 0 here
+        assertEquals(0, someWords.getWordCount());
+
+        for (String word : wordsToAdd){
+            someWords.AddWord(new Word(word));
+        }
+
+        assertEquals(wordsToAdd.length, someWords.getWordCount());
+
+
+    }
+
+
+}
