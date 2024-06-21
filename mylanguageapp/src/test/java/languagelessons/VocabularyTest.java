5a0bac61eea0aab98444aa30b107c0cb556486bf
invalid-cast-exception <159726535+invalid-cast-exception@users.noreply.github.com>
2024-06-22 02:29:06 +0300

    Clean-up imports by removing unused import.

--- mylanguageapp/src/test/java/languagelessons/VocabularyTest.java
+++ mylanguageapp/src/test/java/languagelessons/VocabularyTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import languagelessons.Vocabulary.IfWordAlreadyExists;
