341ae56eef1446b2dbc96b66fb841d20c0b57983
invalid-cast-exception <159726535+invalid-cast-exception@users.noreply.github.com>
2024-06-23 13:51:06 +0300

    Implement WordTransform prototype. Used the TDD cycle to build API and add test coverage for WordTransform. The key focus of this class is to transform a root word to a specific form using a data-driven algorithm, allowing many different transformations to be defined within the application, with the intention of representing lots of the real-life parts of natural languages.

--- mylanguageapp/src/main/java/languagelessons/WordTransform.java
+++ mylanguageapp/src/main/java/languagelessons/WordTransform.java
@@ -1,10 +1,12 @@
 package languagelessons;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import languagelessons.Word.WordTense;
 import languagelessons.Word.WordUsage;
 
 public class WordTransform{
@@ -25,23 +27,24 @@ public class WordTransform{
     //    - Where there are similar but different options possible, it is intended to create a unique WordTransform for each (there will be more duplication of data, but a clearer - approaching 1:1 mapping - between "possible transformation of a word according to natural language" and the WordTransform instances)
     //For more on natural language, see: https://en.wikipedia.org/wiki/Natural_language 
 
-    String textDescription;
-    Map<String, String> knownExamplesOfTransformResults;
+    private String textDescription;
+    private Map<String, String> knownExamplesOfTransformResults;
     
     //Types of transform
-    Map<String, String> replacements;
-    List<String> addToStart;
-    List<String> addToEnd;
-    boolean replacementsAfterAppendsAndPrepends = false;
+    private Map<String, String> replacements;
+    private List<String> addToStart;
+    private List<String> addToEnd;
+    //TODO: implement this option in replacement algorithm
+    private boolean replacementsAfterAppendsAndPrepends = false;
 
     //Applies / Results
-    List<WordUsage> allowedWordUsagesThisTransformCanApplyTo;
-    WordUsage resultingWordUsage;
+    private List<WordUsage> allowedWordUsagesThisTransformCanApplyTo;
+    private WordUsage resultingWordUsage;
 
-    List<String> specialInclusionsForThisRule;
-    List<String> specialExclusionsFromThisRule;
+    private List<String> specialInclusionsForThisRule;
+    private List<String> specialExclusionsFromThisRule;
 
-    protected WordTransform(){
+    public WordTransform(){
 
         this.replacements = new HashMap<String, String>();
         this.knownExamplesOfTransformResults = new HashMap<String,String>();
@@ -89,25 +92,106 @@ public class WordTransform{
     }
 
     //Overload to handle Word as input (for convenience so that calling code does not need to extract the root word)
-    public Word applyTransformationToWord(Word wordToTransform){
+    public static Word applyTransformationToWord(WordTransform transformToUse, Word wordToTransform){
 
-        return applyTransformationToWord(wordToTransform.rootWord());
+        return WordTransform.applyTransformationToWord(transformToUse, wordToTransform.rootWord(), wordToTransform.wordUsage());
     }
 
-    //TODO: could be static pure function
-    public Word applyTransformationToWord(String wordToTransform){
+    public static Word applyTransformationToWord(WordTransform transformToUse, String wordToTransform){
+        return WordTransform.applyTransformationToWord(transformToUse, wordToTransform, WordUsage.Any);
+    }
+
+    public static Word applyTransformationToWord(WordTransform transformToUse, String wordToTransform, WordUsage originalWordUsage){
 
         String resultingWord = wordToTransform;
         //TODO:
         //1. check pre-requisites
         //  - allowedWordUsagesThisTransformCanApplyTo
         //  - special inclusion / exclusion
+        boolean shouldApplyTransform = isWordTransformValidForGivenWord(transformToUse, wordToTransform, originalWordUsage);
+
+        if (shouldApplyTransform){
+            if (transformToUse.replacementsAfterAppendsAndPrepends){
+                
+                resultingWord = WordTransform.applyPrefixesAndSuffixes(transformToUse, resultingWord);
+                resultingWord = WordTransform.applyReplacements(transformToUse, resultingWord);
+
+            }else{
+
+                resultingWord = WordTransform.applyReplacements(transformToUse, resultingWord);
+                resultingWord = WordTransform.applyPrefixesAndSuffixes(transformToUse, resultingWord);
+
+            }
+        }
 
         //depending on order: replacementsAfterAppendsAndPrepends
         //2. apply replacements
         //3. apply append/prepend
+
         
-        return new Word(this.resultingWordUsage, resultingWord);
+        //resulting word usage should not be overriden here if there is an originalWordUsage, and resultingWordUsage was not explcitly set - i.e. has default value
+        //TODO: may want to use a new WordUsage.Unknown to explicitly show where it's not provided
+        WordUsage actualResultingWordUsage = (originalWordUsage != WordUsage.Any && transformToUse.resultingWordUsage == WordUsage.Any) ? originalWordUsage : transformToUse.resultingWordUsage;
+
+        return new Word(actualResultingWordUsage, resultingWord);
+
+    }
+
+    private static String applyReplacements(WordTransform transformToUse, String resultingWord) {
+        
+        String output = resultingWord;
+        for (String thingToReplace : transformToUse.replacements.keySet()){
+
+            output = output.replace(thingToReplace, transformToUse.replacements.get(thingToReplace));
+
+        }
+        return output;
+
+    }
+
+    private static String applyPrefixesAndSuffixes(WordTransform transformToUse, String resultingWord) {
+        
+        String output = resultingWord;
+
+        for (String prefix : transformToUse.addToStart){
+            output = prefix + output;
+        }
+        for (String suffix : transformToUse.addToEnd){
+            output += suffix;
+        }
+
+        return output;
+
+    }
+
+    protected static boolean isWordTransformValidForGivenWord(WordTransform transformToUse, Word wordToTransform) {
+        return isWordTransformValidForGivenWord(transformToUse, wordToTransform.rootWord(), wordToTransform.wordUsage());
+    }
+
+    protected static boolean isWordTransformValidForGivenWord(WordTransform transformToUse, String wordToTransform, WordUsage usageOfWordToTransform) {
+        
+        boolean doesApply = false;
+
+        boolean isSpeciallyIncluded = transformToUse.specialInclusionsForThisRule.contains(wordToTransform);
+        boolean isSpeciallyExcluded = transformToUse.specialExclusionsFromThisRule.contains(wordToTransform);
+        if (isSpeciallyIncluded && isSpeciallyExcluded){
+            //TODO: handle case of misconfigured WordTransform - it should not have the same word as included AND excluded
+        }
+        else if (isSpeciallyIncluded){
+            return true;
+        }
+        else if (isSpeciallyExcluded){
+            return false;
+        }
+
+        if (
+        usageOfWordToTransform == WordUsage.Any 
+        || transformToUse.allowedWordUsagesThisTransformCanApplyTo.contains(WordUsage.Any) 
+        || transformToUse.allowedWordUsagesThisTransformCanApplyTo.contains(usageOfWordToTransform)){
+            doesApply = true;
+        }
+
+        return doesApply;
 
     }
 
@@ -119,7 +203,7 @@ public class WordTransform{
             String inputWord = keysOfKnownExamplesMap[indexOfExampleWord];
             String expectedResultWord = this.knownExamplesOfTransformResults.get(inputWord);
 
-            if (expectedResultWord != this.applyTransformationToWord(inputWord).rootWord()){
+            if (expectedResultWord != WordTransform.applyTransformationToWord(this, inputWord).rootWord()){
                 return false;
             }
 
@@ -129,6 +213,119 @@ public class WordTransform{
         //    - proving this WordTransform works as expected (for the known example words)
         return true;
 
+
+    }
+
+    public String getTextDescription() {
+        return this.textDescription;
+    }
+
+    public int getReplacementRuleCount() {
+        return this.replacements.size();
+    }
+
+    public int getKnownExampleCount() {
+        return this.knownExamplesOfTransformResults.size();
+    }
+
+    public int getCountOfPrefixes() {
+        return this.addToStart.size();
+    }
+    public int getCountOfSuffixes() {
+        return this.addToEnd.size();
+    }
+
+    public int getCountOfSpecialExclusionsFromThisRule() {
+        return this.specialExclusionsFromThisRule.size();
+    }
+    public int getCountOfSpecialInclusionsForThisRule() {
+        return this.specialInclusionsForThisRule.size();
+    }
+
+    public void setTextDescription(String textDescription) {
+        this.textDescription = textDescription;
+    }
+
+    public String getExcludedWordsAsCommaDelimitedString() {
+        return this.getSpecialCaseWordListAsCommaDelimitedConcatenatedString(SpecialCaseListType.ExclusionsFrom);
+    }
+
+    public String getIncludedWordsAsCommaDelimitedString() {
+        return this.getSpecialCaseWordListAsCommaDelimitedConcatenatedString(SpecialCaseListType.InclusionsTo);
+    }
+
+    private enum SpecialCaseListType{
+        InclusionsTo, ExclusionsFrom
+    };
+
+    public String getSpecialCaseWordListAsCommaDelimitedConcatenatedString(SpecialCaseListType typeOfList){
+
+        StringBuilder concatenatedSpecialCaseWords = new StringBuilder();
+
+        List<String> temporaryCopyOfSpecialCaseWords = new ArrayList<String>();
+        
+        switch (typeOfList){
+            case InclusionsTo:
+                temporaryCopyOfSpecialCaseWords.addAll(this.specialInclusionsForThisRule);
+                break;
+            case ExclusionsFrom:
+                temporaryCopyOfSpecialCaseWords.addAll(this.specialExclusionsFromThisRule);
+                break;
+            default:
+                break;
+        }
+
+        //Basic comparator, using default comparison from String, to sort alphabetically
+        temporaryCopyOfSpecialCaseWords.sort( (String a, String b) -> {
+            return a.compareTo(b);
+        } );
+
+        
+        for (int indexOfCurrentSpecialCaseWord = 0; indexOfCurrentSpecialCaseWord < temporaryCopyOfSpecialCaseWords.size(); indexOfCurrentSpecialCaseWord++){
+            String currentSpecialCaseWord = temporaryCopyOfSpecialCaseWords.get(indexOfCurrentSpecialCaseWord);
+
+            concatenatedSpecialCaseWords.append(currentSpecialCaseWord);
+            if (indexOfCurrentSpecialCaseWord != temporaryCopyOfSpecialCaseWords.size()-1){
+                concatenatedSpecialCaseWords.append(",");
+            }
+
+        }
+
+        return concatenatedSpecialCaseWords.toString();
+
+
+    }
+
+    /*
+    private List<WordUsage> allowedWordUsagesThisTransformCanApplyTo;
+    private WordUsage resultingWordUsage;
+     */
+
+    public void setResultingWordUsage(WordUsage wordUsageIfTransformationSuccessfullyAppliedToWord){
+
+        this.resultingWordUsage = wordUsageIfTransformationSuccessfullyAppliedToWord;
+
+    }
+
+    public void addWordUsageThisRuleAppliesTo(WordUsage wordUsageThisRuleCanApplyTo){
+
+        //NOTE: if word usage being added is "WordUsage.Any" we COULD delete all other entries as an optimisation 
+        //    - but since doing so would destroy the "history" and the resulting behavior will be the same: prefer to add "WordUsage.Any" in the usual way
+
+        //stop using the default setting if we are adding a new word usage
+        if (this.allowedWordUsagesThisTransformCanApplyTo.size() == 1 && this.allowedWordUsagesThisTransformCanApplyTo.get(0) == WordUsage.Any){
+            if (wordUsageThisRuleCanApplyTo == WordUsage.Any) {
+                //we have the default list - which is 1 entry of WordUsage.Any - and the caller is adding WordUsage.Any
+                //for this case, we can just skip adding and early-out here
+                //    - because the state of the list is already as requested
+                //    - this assumes user just wants WordUsage.Any present, and does not care if the list contains 1 or 2 entries of it
+                return;
+            }else{
+                this.allowedWordUsagesThisTransformCanApplyTo.clear();
+            }
+        }
+
+        this.allowedWordUsagesThisTransformCanApplyTo.add(wordUsageThisRuleCanApplyTo);
 
     }
 
--- mylanguageapp/src/test/java/languagelessons/WordTransformTest.java
+++ mylanguageapp/src/test/java/languagelessons/WordTransformTest.java
@@ -1,18 +1,277 @@
 package languagelessons;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import languagelessons.Word.WordUsage;
+
 public class WordTransformTest {
 
-
-    //TEST CASES
-    // - constructors - WordTransform (textDescription), WordTransform()
     // - bool validateKnownExamples
-    // - Word applyTransformationToWord (string)
-    // - addSpecialExclusionForGivenWord
-    // - addSpecialInclusionForGivenWord
-    // - addTextToAppend
-    // - addTextToPrepend
-    // - addKnownReplacementExample
-    // - addReplacementRule
-    // - 
+    
+
+    WordTransform myTestWordTransform;
+
+    @BeforeEach void SetupDefaultWordTransform(){
+        myTestWordTransform = new WordTransform("A default WordTransform constructed by test setup.");
+
+    }
+
+    @Test void canConstructWordTransformWithDefaultConstructor(){
+        
+        assertNotNull(new WordTransform());
+
+    }
+
+    @Test void canConstructWordTransformGivenATextDescription(){
+        
+        String textDescription = "Add -ed suffix to make a verb past tense";
+
+        WordTransform t = new WordTransform(textDescription);
+        assertNotNull(t);
+
+        assertEquals(textDescription, t.getTextDescription());
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={
+        "a,b,c,d,e,f"
+        //bake -> baked
+        , "e,ed"
+        //better->be"tt"est, greater->greatest
+        , "er,est"
+    })
+    void canAddReplacementRuleToWordTransform(String rulesAsCommaDelimitedString){
+
+        //each pair of strings represents a single WordTransform "Replacement Rule"
+        String[] testInputRules = rulesAsCommaDelimitedString.split(",");
+
+        //defensive check to stop misconfigured test issues - must be provided string pairs to work
+        assertEquals(0, testInputRules.length % 2);
+
+        //defensive check - should start from default instance with 0 rules established
+        assertEquals(0, myTestWordTransform.getReplacementRuleCount());
+
+        for (int indexOfFirstStringInRule = 0; indexOfFirstStringInRule < testInputRules.length; indexOfFirstStringInRule+=2){
+
+            myTestWordTransform.addReplacementRule(testInputRules[indexOfFirstStringInRule], testInputRules[indexOfFirstStringInRule+1]);
+
+        }
+        
+        assertEquals(testInputRules.length / 2, myTestWordTransform.getReplacementRuleCount());
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={
+        "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+        , "bake,baked,fake,faked"
+        , "better,best,greater,greatest"
+    })
+    void canAddKnownReplacementExample(String knownExamplesAsCommaDelimitedString){
+
+        //each pair of strings represents a single WordTransform "Replacement Rule"
+        String[] testInputKnownExamples = knownExamplesAsCommaDelimitedString.split(",");
+
+        //defensive check to stop misconfigured test issues - must be provided string pairs to work
+        assertEquals(0, testInputKnownExamples.length % 2);
+
+        //defensive check - should start from default instance with 0 rules established
+        assertEquals(0, myTestWordTransform.getKnownExampleCount());
+
+        for (int indexOfFirstStringInRule = 0; indexOfFirstStringInRule < testInputKnownExamples.length; indexOfFirstStringInRule+=2){
+
+            myTestWordTransform.addKnownReplacementExample(testInputKnownExamples[indexOfFirstStringInRule], testInputKnownExamples[indexOfFirstStringInRule+1]);
+
+        }
+        
+        assertEquals(testInputKnownExamples.length / 2, myTestWordTransform.getKnownExampleCount());
+
+    }
+
+    @Test void canAddTextToBePrependedByWordTransformToTheRootWord(){
+
+        assertEquals(0, myTestWordTransform.getCountOfPrefixes());
+
+        final String[] wordsToPrepend = new String[]{
+            "Be"
+            , "Re"
+            , "Un"
+            , "Anti"
+            , "Dis"
+            , "Con"
+            , "Pre"
+            , "Pro"
+            , "Hyper"
+            , "Hypo"
+        };
+
+        for (String currentPrefix : wordsToPrepend){
+            myTestWordTransform.addTextToPrepend(currentPrefix);
+        }
+
+        assertEquals(wordsToPrepend.length, myTestWordTransform.getCountOfPrefixes());
+
+    }
+
+
+    @Test void canAddTextToBeAppendedByWordTransformToTheRootWord(){
+
+        assertEquals(0, myTestWordTransform.getCountOfSuffixes());
+
+        final String[] wordsToAppend = new String[]{
+            "tion"
+            , "ed"
+            , "ism"
+            , "ian"
+            , "ist"
+            , "ly"
+        };
+
+        for (String currentSuffix : wordsToAppend){
+            myTestWordTransform.addTextToAppend(currentSuffix);
+        }
+
+        assertEquals(wordsToAppend.length, myTestWordTransform.getCountOfSuffixes());
+
+    }
+
+    @Test void canAddSpecialExclusionForGivenWord(){
+
+        assertEquals(0, myTestWordTransform.getCountOfSpecialExclusionsFromThisRule());
+
+        //not needed for the unit test but helps illustrate the usage
+        //TODO: replacement rule will also need an option to control how replacement gets done - just endings? anywhere in string? others?
+        myTestWordTransform.addReplacementRule("ke", "ked");
+        //also not needed but good example
+        myTestWordTransform.setTextDescription("Change words ending 'ke' to past tense, in English.");
+
+        //take becomes taken, not taked
+        myTestWordTransform.addSpecialExclusionForGivenWord("take");
+        //make becomes made, not maked
+        myTestWordTransform.addSpecialExclusionForGivenWord("make");
+        //forsake becomes forsook, not forsaked
+        myTestWordTransform.addSpecialExclusionForGivenWord("forsake");
+
+
+
+        assertEquals(3, myTestWordTransform.getCountOfSpecialExclusionsFromThisRule());
+
+        //assumes order will be deterministic using alphabetic sorting, because that's the more predicatable than the adding order
+        assertEquals("forsake,make,take", myTestWordTransform.getExcludedWordsAsCommaDelimitedString());
+
+
+    }
+
+    @Test void canAddSpecialInclusionForGivenWord(){
+
+        assertEquals(0, myTestWordTransform.getCountOfSpecialInclusionsForThisRule());
+
+        //not needed for the unit test but helps illustrate the usage
+        //TODO: replacement rule will also need an option to control how replacement gets done - just endings? anywhere in string? others?
+        myTestWordTransform.addReplacementRule("ke", "ked");
+        //also not needed but good example
+        myTestWordTransform.setTextDescription("Change words ending 'ke' to past tense, in English.");
+
+        //take becomes taken, not taked
+        myTestWordTransform.addSpecialInclusionForGivenWord("take");
+        //make becomes made, not maked
+        myTestWordTransform.addSpecialInclusionForGivenWord("make");
+        //forsake becomes forsook, not forsaked
+        myTestWordTransform.addSpecialInclusionForGivenWord("forsake");
+
+
+
+        assertEquals(3, myTestWordTransform.getCountOfSpecialInclusionsForThisRule());
+
+        //assumes order will be deterministic using alphabetic sorting, because that's the more predicatable than the adding order
+        assertEquals("forsake,make,take", myTestWordTransform.getIncludedWordsAsCommaDelimitedString());
+
+
+    }
+
+    @Test void canDetermineWordTransformDoesNotApplyForGivenWordAccordingToWordUsage(){
+
+        myTestWordTransform.addWordUsageThisRuleAppliesTo(WordUsage.Verb);
+
+        Word noun = new Word(WordUsage.Noun, "Train");
+
+        assertFalse(WordTransform.isWordTransformValidForGivenWord(myTestWordTransform, noun));
+
+    }
+    @Test void canDetermineWordTransformDoesApplyForGivenWordAccordingToWordUsage(){
+
+        myTestWordTransform.addWordUsageThisRuleAppliesTo(WordUsage.Verb);
+
+        Word verb = new Word(WordUsage.Verb, "Climb");
+
+        assertTrue(WordTransform.isWordTransformValidForGivenWord(myTestWordTransform, verb));
+
+
+    }
+    
+    @Test void canDetermineWordTransformDoesNotApplyForGivenWordAccordingToSpecialExclusions(){
+
+        myTestWordTransform.addWordUsageThisRuleAppliesTo(WordUsage.Verb);
+        myTestWordTransform.addSpecialExclusionForGivenWord("Climb");
+        Word verb = new Word(WordUsage.Verb, "Climb");
+
+        assertFalse(WordTransform.isWordTransformValidForGivenWord(myTestWordTransform, verb));
+
+    }
+
+    @Test void canDetermineWordTransformDoesApplyForGivenWordAccordingToSpecialInclusions(){
+
+        myTestWordTransform.addWordUsageThisRuleAppliesTo(WordUsage.Verb);
+        myTestWordTransform.addSpecialInclusionForGivenWord("Train");
+
+        Word noun = new Word(WordUsage.Noun, "Train");
+
+        assertTrue(WordTransform.isWordTransformValidForGivenWord(myTestWordTransform, noun));
+
+
+    }
+
+
+    //TODO:
+    //Test for String applyReplacements(WordTransform transformToUse, String resultingWord)
+    //Test for WordTransform.applyPrefixesAndSuffixes(WordTransform transformToUse, String resultingWord)
+
+    @Test void canApplyTransformToGivenString(){
+
+        //setup the transform for the test case
+        //    - a rule to pluralise a singular noun by adding "s" to the end
+        myTestWordTransform.setTextDescription("Singular to plural, in English");
+        myTestWordTransform.addKnownReplacementExample("wing", "wings");
+        myTestWordTransform.addKnownReplacementExample("kettle", "kettles");
+        myTestWordTransform.addWordUsageThisRuleAppliesTo(WordUsage.Noun);
+        myTestWordTransform.addTextToAppend("s");
+        //Acting as a mock user, we know that Sky becomes skies, not skys - so we can add an exclusion here
+        myTestWordTransform.addSpecialExclusionForGivenWord("Sky");
+
+        Word transformedWord = WordTransform.applyTransformationToWord(myTestWordTransform, "Kite");
+
+        assertEquals("Kites", transformedWord.rootWord());
+
+    
+        transformedWord = WordTransform.applyTransformationToWord(myTestWordTransform, "Sky");
+
+        assertEquals("Sky", transformedWord.rootWord());
+
+        //TODO: run tests, commit latest progress
+
+    }
+
+
+
+
 
 }
