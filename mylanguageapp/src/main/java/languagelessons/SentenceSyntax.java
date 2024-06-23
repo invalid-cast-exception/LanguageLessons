efd21c2cb831ce76c485e638f5da4cc3acbd720c
invalid-cast-exception <159726535+invalid-cast-exception@users.noreply.github.com>
2024-06-23 13:54:56 +0300

    Add empty class for planned API, with code comments explaining planned design and functionality.

---
+++ mylanguageapp/src/main/java/languagelessons/SentenceSyntax.java
@@ -0,0 +1,15 @@
+package languagelessons;
+
+
+//TODO: this class will specify a syntax for a sentence
+// For use in various places, such as:
+// - generate a sentence by replacing words with words from vocabularies
+// - generating quizzes (e.g. what form of the word fits in the sentence?)
+// - ... 
+//Example Syntax:
+//[quantifier] [adjective] [noun, subject] [adverb] [verb] [preposition] [quantifier] [noun, object] [verb, infinitive] [quantifier] [noun]
+//Example sentence from syntax:
+//A happy goat joyfully jumps over a fence to eat some carrots
+public class SentenceSyntax {
+
+}
---
+++ mylanguageapp/src/main/java/languagelessons/WordConjugator.java
@@ -0,0 +1,19 @@
+package languagelessons;
+
+//TODO: this class will contain an array of WordTransforms
+//This allows very specific WordTransforms to be created 
+// - maybe even affecting only 1 word 
+// - but still be connected to WordTransforms that do the same transform, but configured for different input words or slightly different results
+//Example:
+//description: pluralise a singular noun, in English
+//WordTransform[0] = y->ies rule, as in Sky -> Skies
+//WordTransform[1] = e->es rule, as in Bike -> Bikes
+//WordTransform[2] = consonant->add s, as in Chair -> Chairs, Troll -> Trolls
+//WordTransform[3] = a->ae rule, as in Antenna -> Antennae
+//NOTES: 
+// - an individual WordTransform corresponds to the action taken on one word to change it's form (and if the rule is generic for multiple words then it will also be used for those words too)
+// - a GroupOfSimilarWordTransforms corresponds to a pattern - multiple WordTransforms collected together because they all achieve the same goal (but for different input/output words)
+
+public class WordConjugator {
+
+}
